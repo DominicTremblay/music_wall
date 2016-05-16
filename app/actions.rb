cc30e557d80eec32f1e5cc372b77b5a7eec34ad4
Dominic Tremblay <dominictremblay24@gmail.com>
2016-05-16 21:53:58 +0000

    initial commit

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,33 @@
 get '/' do
   erb :index
 end
+
+
+get '/songs' do
+  @songs = Song.all
+  erb :songs
+end
+
+get '/albums' do
+  @albums = Album.all
+  erb :albums
+end
+
+get '/new' do
+  erb :new
+end
+
+
+post '/songs' do
+  @song = Song.new(
+    name:   params[:name],
+    artist: params[:artist],
+    likes:  params[:likes]
+  )
+  if @songs.save
+    redirect '/songs'
+  else
+    erb :'/new'
+  end
+end
+
