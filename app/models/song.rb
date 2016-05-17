c8e75fc436416289d93ba935d5265db8779f9ca4
Dominic Tremblay <dominictremblay24@gmail.com>
2016-05-17 19:11:13 +0000

    git fixed

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 140 }
   validates :artist, presence: true, length: { maximum: 140 }
   validates :likes, presence: true, numericality: { only_integer: true }
+  
 end
